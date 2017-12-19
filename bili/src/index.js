aaac5760c1f85cd1037bc003c67552e0e2f651e3
web前端-曾佑森 <2356281422@qq.com>
2017-12-19 17:22:51 +0800

    配置文件

--- bili/src/index.js
+++ bili/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
 import './index.css';
 import './reset.css';
 import Nav from './component/nav_title';
-import showCal from './reducers/carousel';
 import Index from './component/recommend';
 import Comic from './containers/comic/comic';
 
 
-let store = createStore(showCal);
 class Cover extends React.Component{
     constructor(props){
         super(props);
